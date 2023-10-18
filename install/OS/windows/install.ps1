819cb29d97b834daa6fed9758b755d488f46a81b
Nick <nick22985@hotmail.com>
2023-10-19 09:47:08 +1000

    Update install.ps1

--- install/OS/windows/install.ps1
+++ install/OS/windows/install.ps1
@@ -3,23 +3,6 @@ if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]:
 	Exit
 }
 
-# Install winget
-# get latest download url
-$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
-        Select-Object -ExpandProperty "assets" |
-        Where-Object "browser_download_url" -Match '.msixbundle' |
-        Select-Object -ExpandProperty "browser_download_url"
-
-# download
-Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
-
-# install
-Add-AppxPackage -Path "Setup.msix"
-
-# delete file
-Remove-Item "Setup.msix"
-
 $USERPROFILE = Get-Content $PROFILE
 
 $configFunction = 'function config {& "$env:ProgramFiles\Git\bin\git.exe" --git-dir="$env:userprofile/.dotfiles/" --work-tree="$env:userprofile/" $args}'

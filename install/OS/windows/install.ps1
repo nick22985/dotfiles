b33864ac08f940f49c61df3dbd777d1106f99d37
Nick <nick22985@hotmail.com>
2023-10-19 09:48:04 +1000

    Update install.ps1

--- install/OS/windows/install.ps1
+++ install/OS/windows/install.ps1
@@ -5,17 +5,6 @@ if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]:
 
 $USERPROFILE = Get-Content $PROFILE
 
-$configFunction = 'function config {& "$env:ProgramFiles\Git\bin\git.exe" --git-dir="$env:userprofile/.dotfiles/" --work-tree="$env:userprofile/" $args}'
-
-if ($USERPROFILE -contains $configFunction) {
-	Write-host "config function already exists"
-} else {
-	Write-host "Installing config function"
-	Add-Content -Path $PROFILE -Value "`n$configFunction"
- 	# Refreshes the path variable without needing to restart powershell
-	$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-}
-
 function wingetApplication {
 	param(
 		$applicationId

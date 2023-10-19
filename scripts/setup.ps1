eaa7e47056ce01a039c1de411e73a4a8821052af
Nick <nick22985@hotmail.com>
2023-10-19 10:18:39 +1000

    Update setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -7,7 +7,6 @@ if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]:
 }
 
 # Install winget
-$progressPreference = 'silentlyContinue'
 Write-Information "Downloading WinGet and its dependencies..."
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
